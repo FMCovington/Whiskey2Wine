6e31851b886335ee064e4d0638523d1f7750c7ce
F. M. (Mike) Covington, PhD, MCT, PCPP <52182128+FMCovington@users.noreply.github.com>
2024-03-28 14:05:21 -0500

    Add naive test file
    Simple test file added to repository as demonstration of concept only

---
+++ test_whiskey_to_wine.py
@@ -0,0 +1,25 @@
+# Simple unit test of demo_dilution_example_using_C1V1_eq_C2V2_toTest.py
+
+from whiskey_to_wine import whiskey2wine
+
+
+def test_whiskey2wine(proof=80, ounces=1.5):
+    result = whiskey2wine(proof, ounces)
+    print("Result: ", result)
+    print(proof, ounces, result)
+    assert result[-1][-4:] == 'True', f"Result does not equal expectation: {result[-1][-4:]}"
+
+
+# test defaults
+print('Testing defaults (80,1.5):')
+test_whiskey2wine()
+print()
+
+# test reasonable combinations of proof and volume
+print('Testing all reasonable values for each parameter:')
+for prf in range(40, 195):
+    for vol in range(1, 10):
+        test_whiskey2wine(prf, vol)
+
+# Only prints if no assertion errors.
+print("\n100% Success")
